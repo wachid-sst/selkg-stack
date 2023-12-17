fe050cb8239243116bfc4ab68a77e3bc46020529
wachid-sst <wachid-sst@users.noreply.github.com>
2023-12-17 13:42:56 +0700

    Update docker-entrypoint.sh

--- suricata/docker-entrypoint.sh
+++ suricata/docker-entrypoint.sh
@@ -68,4 +68,4 @@ echo ${SURICATA_OPTIONS}
 ls -l /etc/suricata/
 
 #exec /usr/bin/suricata ${ARGS} ${SURICATA_OPTIONS} $@
-exec /usr/bin/suricata -c suricata.yaml -i ens160 -vvv $@
+exec /usr/bin/suricata -i ens160 -vvv $@
